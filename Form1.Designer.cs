faa6eac8dfae64b81792e1db630848bb30c76f4f
KyogreOfTime <75351912+KyogreOfTime@users.noreply.github.com>
2020-12-18 22:54:00 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace FizzBuzzV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FizzBuzzButton = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FizzBuzzButton
+            // 
+            this.FizzBuzzButton.Location = new System.Drawing.Point(246, 50);
+            this.FizzBuzzButton.Name = "FizzBuzzButton";
+            this.FizzBuzzButton.Size = new System.Drawing.Size(75, 23);
+            this.FizzBuzzButton.TabIndex = 0;
+            this.FizzBuzzButton.Text = "FizzBuzz!";
+            this.FizzBuzzButton.UseVisualStyleBackColor = true;
+            this.FizzBuzzButton.Click += new System.EventHandler(this.FizzBuzzButton_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.Location = new System.Drawing.Point(61, 88);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(448, 163);
+            this.OutputLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 263);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.FizzBuzzButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button FizzBuzzButton;
+        private System.Windows.Forms.Label OutputLabel;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzzV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FizzBuzzButton_Click(object sender, EventArgs e)
+        {
+            var stringArray = new string[100];
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if ((i + 1) % 3 != 0 && (i + 1) % 5 != 0)
+                {
+                    stringArray[i] = (i + 1).ToString();
+                }
+                if ((i + 1) % 3 == 0)
+                {
+                    stringArray[i] += "Fizz";
+                }
+                if ((i + 1) % 5 == 0)
+                {
+                    stringArray[i] += "Buzz";
+                }
+            }
+            
+            OutputLabel.Text = string.Join(", ", stringArray);
+        }
+    }
+}
